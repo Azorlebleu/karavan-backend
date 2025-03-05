1cf696a4ddd627d73517cde1a5ea57c4893d1ec9
Pedro <pedro.lelievre@gmail.com>
2025-03-05 23:44:06 +0100

    Reverted detailed login

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
+
 from fastapi import FastAPI
 from app.models.database import database
 from app.repository.room import init_redis as redis_room_init
 from app.repository.chat import init_redis as redis_chat_init
 
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.requests import Request
-import logging
 
 from app.api.websocket import router as websocket_router
 from app.api.room import router as room_router
@@ -22,37 +20,6 @@ from app.logger import logger
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")  # Default to localhost if not set
 
 app = FastAPI()
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-
-# Custom Middleware to log request details
-class RequestLoggingMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        # Log the details of the incoming request
-        logging.info(f"Request Method: {request.method}")
-        logging.info(f"Request URL: {request.url}")
-        logging.info(f"Request Headers: {request.headers}")
-        
-        # If there are query parameters, log them too
-        if request.query_params:
-            logging.info(f"Query Parameters: {request.query_params}")
-
-        # Log the body if needed (but beware it might be consumed)
-        body = await request.body()
-        if body:
-            logging.info(f"Request Body: {body.decode()}")
-
-        # Proceed with the request
-        response = await call_next(request)
-        
-        # Optionally log the response status code
-        logging.info(f"Response Status Code: {response.status_code}")
-        
-        return response
-
-# Add the middleware to the FastAPI app
-app.add_middleware(RequestLoggingMiddleware)
 
 # Handle CORS
 app.add_middleware(
@@ -82,3 +49,4 @@ app.include_router(game_router)
 @app.get("/")
 def read_root():
     return {"message": "KaraVan backend is running!"}
+
